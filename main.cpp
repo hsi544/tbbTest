05f5e4e5c674244bd41b264cd7ae6ed2a570d985
Tarik Saidani <tsaidani@tariks-mbp.onshore.pgs.com>
2019-08-14 11:19:54 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "tbb/task_scheduler_init.h"
+
+
+using namespace tbb;
+
+int main()
+{
+
+   task_scheduler_init init;
+
+   return 0;
+
+
+}
---
+++ parallel_for/parfor.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <cmath>
+#include "tbb/blocked_range.h"
+#include "tbb/parallel_for.h"
+#include "tbb/tick_count.h"
+
+using namespace tbb;
+
+void Foo(float &a)
+{
+
+    //a += 2;
+    a = cos(a*a);
+
+}
+
+class ApplyFoo {
+   float *const my_a;
+public:
+   void operator()(const blocked_range<size_t>&r) const {
+      float *a = my_a;
+      for(size_t i = r.begin(); i!= r.end(); i++)
+         Foo(a[i]);
+   }
+
+   ApplyFoo(float a[]):my_a(a){}
+
+};
+
+
+
+
+void SerialApplyFoo(float a[], size_t n)
+{
+
+   for( size_t i= 0; i < n ; ++i) {
+
+     Foo(a[i]);
+
+   }
+
+}
+
+
+void ParallelApplyFoo(float a[], size_t n) {
+
+   parallel_for(blocked_range<size_t>(0, n, 1), ApplyFoo(a));
+
+}
+
+int main()
+{
+   size_t sz = 1024*1024*512;
+
+   std::vector<float> a(sz, 0.0f);   
+
+   for(int i = 0 ; i < a.size() ; i++) {
+
+      a[i] = rand()/INT_MAX;
+   }
+
+   tick_count t0 = tick_count::now();
+
+   //SerialApplyFoo(a.data(), a.size());
+   ParallelApplyFoo(a.data(), a.size());
+
+   tick_count t1 = tick_count::now();
+
+   std::cout << "elapsed time: " << (t1-t0).seconds() << " sec" << std::endl;
+
+   return 0;
+}
